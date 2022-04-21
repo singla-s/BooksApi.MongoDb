7674cd3bc05e8387c72431a0db500131f66adef2
sinnisingla <sinnisingla@outlook.com>
2022-04-21 10:55:41 +0530

    Add project files.

---
+++ BooksApi.MongoDb/Models/Book.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksApi.MongoDb.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int _Id { get; set; }
+
+        [BsonElement("id")]
+        public string Id { get; set; }
+
+        [StringLength(100,MinimumLength = 5)]
+        [BsonElement("title")]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [BsonElement("releaseDate")]
+        public DateTime ReleaseDate { get; set; }
+
+        [BsonElement("price")]
+        public double Price { get; set; }
+
+        [Range(20,1000)]
+        [BsonElement("author")]
+        public string Author { get; set; }
+    }
+}
