c6cf415d54e286ce3940e27873caf5da0631db2d
sinnisingla <sinnisingla@outlook.com>
2022-04-25 12:34:59 +0530

    feat: change signalr notification format

--- BooksApi.MongoDb/Hubs/BooksNotificationHub.cs
+++ BooksApi.MongoDb/Hubs/BooksNotificationHub.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using BooksApi.MongoDb.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Threading.Tasks;
 
 namespace BooksApi.MongoDb.Hubs
@@ -11,21 +13,21 @@ namespace BooksApi.MongoDb.Hubs
         public async void subscribeToNewBookRelease(string user, string message)
         {
             callers = Clients;
-            string strBookJson = @"{
-                '_Id': '6260e73bc363928f414c7765',
-                'id': 121,
-                'title': 'Finish',
-                'releaseDate': '2001-08-14T18:30:00Z',
-                'price': 66.66,
-                'author': 'Jon Acuff'
-              }";
-            await NotifyBookRelease(strBookJson);
+            Book newBook = new Book();
+            newBook._Id = "6260e73bc363928f414c7769";
+            newBook.Id = 121;
+            newBook.Title = "Finish";
+            newBook.ReleaseDate = DateTime.Now;
+            newBook.Price = 66.66;
+            newBook.Author = "Jon Acuff";
+
+            await NotifyBookRelease(newBook);
         }
 
         //notifier method
-        public async static Task NotifyBookRelease(string strNewBook)
+        public async static Task NotifyBookRelease(Book newBook)
         {
-            await callers.All.SendAsync($"NewBookNotification", strNewBook);
+            await callers.All.SendAsync("NewBookNotification", newBook);
         }
     }
 }
